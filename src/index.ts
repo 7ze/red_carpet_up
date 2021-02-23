9d72a634163724771eebca8f166a9babcdfec2c2
The_philosopher <shinudonney@tuta.io>
2021-02-23 20:55:38 +0530

    Set up a basic fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import fastify from 'fastify';
+
+const app = fastify({ logger: true });
+
+app.get('/', async () => {
+  return { hello: 'world' };
+});
+
+const port = process.env.PORT ?? 8080;
+
+const start = async () => {
+  try {
+    await app.listen(port, '0.0.0.0');
+  } catch (e) {
+    app.log.error(e);
+    process.exit(1);
+  }
+};
+start();

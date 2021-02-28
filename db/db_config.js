da554e7ac38136568e7b6e447e7862da1cd84a98
The_philosopher <shinudonney@tuta.io>
2021-02-28 18:59:24 +0530

    Add user registration logic

--- db/db_config.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = new Pool({
-  user: "postgres",
-  database: "loan_db",
-  host: "localhost",
-  port: 5432,
-});
--- services/userAuthentication.js
+++ services/userAuthentication.js
@@ -1,12 +1,32 @@
 const bcrypt = require("bcryptjs");
+const db = require("../db");
 
 const hash_password = async (password) => {
   const saltRounds = 10;
   return await bcrypt.hash(password, saltRounds);
 };
 
-const userExists = () => {
-  // check if the user exists
+const userExists = async (username) => {
+  const { rowCount } = await db.query(
+    "select * from users where username = $1",
+    username
+  );
+  return rowCount > 0 ? true : false;
 };
 
-module.exports = {};
+const registerUser = async (user) => {
+  const [username, name, password, role] = user;
+  if (userExists) return { message: "Bad request." };
+  else {
+    const hashed_password = await hash_password(password);
+    return await db.query(
+      "insert into users (username, name, password, role) values ($1, $2, $3, $4)",
+      username,
+      name,
+      hashed_password,
+      role
+    );
+  }
+};
+
+module.exports = { registerUser };
